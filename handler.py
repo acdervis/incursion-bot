d1cf7156725d0973e7708255d575e5d900766482
acdervis <acdervis3@gmail.com>
2015-01-15 15:17:35 +0200

    Initial commit.

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import incursion
 import json
 from flask import Response
 
+
 def process(data):
 
     print type(data)
@@ -11,18 +12,37 @@ def process(data):
     print data
 
     if data == '!inc':
-        text = ""
-        # if len(data.split()) >= 2:
-        #     print data.split()[1]
-        # else:
+        constellations = []
         incursions = incursion.getIncursions(short=True)
         for inc in incursions:
-            text += inc.constellation.name + ', '
+            constellations.append(inc.constellation.name)
 
-        d = {'text': text}
+        constellations.sort()
+        d = {'text': ', '.join(constellations)}
         response = Response(json.dumps(d), mimetype='text/json')
 
         print response.get_data()
         return response
     else:
-        return "No data provided."
+        if data.split()[0] == '!inc':
+            arg = data.split()[1]
+            incursions = incursion.getIncursions()
+            found = False
+            for inc in incursions:
+                if arg == inc.constellation.name:
+                    d = {
+                        'name': inc.constellation.name,
+                        'state': inc.state,
+                        'influence': inc.influence,
+                        'sysdata': inc.getSystemTypes(),
+                        'pockets': inc.getBestClusters(),
+                    }
+                    found = True
+                    body = {'text': incursion.template_incursioninformation.substitute(d)}
+            if found:
+                response = Response(json.dumps(d), mimetype='text/json')
+            else:
+                return "Constellation not found under active incursions."
+        else:
+            return "Bad request."
+
